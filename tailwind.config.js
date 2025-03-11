42d0edb46abc6d4d269183244f0eb667b997b2ff
Stacy Druart <stacy@StacyDruartMSI>
2025-03-11 18:29:32 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  content: [
+    './index.html',  // Assurez-vous d'inclure le chemin de votre fichier HTML
+    './src/**/*.{html,js}',  // Si vous avez des fichiers JS ou autres sources
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
