2ae812bd58578cf1d3c190efec011bdebb218749
Stacy Druart <stacy@StacyDruartMSI>
2025-03-20 13:55:31 +0100

    interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
-
 module.exports = {
-  content: [
-    './index.html',  // Assurez-vous d'inclure le chemin de votre fichier HTML
-    './src/**/*.{html,js}',  // Si vous avez des fichiers JS ou autres sources
-  ],
+  content: ["./*.html"], // Ajoute tes fichiers HTML ici
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
